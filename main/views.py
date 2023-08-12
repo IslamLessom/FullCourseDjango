fd303bcd8f7855b66659be5bdd2c7312da195c48
IslamProg <74923378+IslamLessom@users.noreply.github.com>
2023-08-12 15:58:10 +0300

    Add files via upload

--- main/views.py
+++ main/views.py
@@ -3,7 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница',
+        'values': ['some', 'hello', 'world']
+    }
+
+
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
