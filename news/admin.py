8aa480d063dc9de239758de2606be93203162461
IslamProg <74923378+IslamLessom@users.noreply.github.com>
2023-08-15 17:38:34 +0300

    Add files via upload

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Artiles
+from .models import Articles
 
 # Register your models here.
-admin.site.register(Artiles)
+admin.site.register(Articles)
--- news/models.py
+++ news/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
 
-
-class Artiles(models.Model):
-    title = models.CharField('Название', max_length=50,default='Новость')
+class Articles(models.Model):
+    title = models.CharField('Название', max_length=50, default='Новость')
     anons = models.CharField('Анонс', max_length=250, default='Анонс')
     full_text = models.TextField("Статья")
     date = models.DateTimeField("Дата публикации")
-
+    objects = models.Manager()
     def __str__(self):
         return self.title
 
